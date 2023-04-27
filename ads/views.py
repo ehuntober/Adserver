e88c7a341bea4ee3a0c44cfffd497a6b74fb685e
ehuntober <ehuntercrypto@gmail.com>
2023-04-27 16:42:30 +0000

    listing ads done

--- ads/views.py
+++ ads/views.py
@@ -9,4 +9,8 @@ from ads.serializers import AdSerializer
 
 class AdListCreateAPIView(generics.ListCreateAPIView):
     queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
+    def perform_create(self, serializer):
+        serializer.save(advertiser=self.request.user)
