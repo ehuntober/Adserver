6423875756f0d402ea03c0401311f6dc6f243ddd
ehuntober <ehuntercrypto@gmail.com>
2023-04-27 16:24:47 +0000

    models created

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User 
+
+class Ad(mdoels.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="ads")
+    url = models.URLField()
+    adverster = models.ForeignKey(User, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
