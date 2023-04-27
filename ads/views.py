7ada4143f4c7ce3bd8fa72b319879c6a6c8c5c46
ehuntober <ehuntercrypto@gmail.com>
2023-04-27 16:37:18 +0000

    created the first view of the ads

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from ads.models import Ad 
+from ads.serializers import AdSerializer
+
+class AdListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Ad.objects.all()
+    
