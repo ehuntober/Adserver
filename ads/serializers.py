5761dd78199dd9238f0cb4622b419c52d4039c39
ehuntober <ehuntercrypto@gmail.com>
2023-04-28 07:35:17 +0000

    user profile serialized

--- ads/serializers.py
+++ ads/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from ads.models import Ad 
+from ads.models import Ad , UserProfile
 
 class AdSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Ad
+        fields = '__all__'
+        
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = UserProfile
         fields = '__all__'
