9d15f7714de27722c7c3a0bb1684762ec8fc6468
ehuntober <ehuntercrypto@gmail.com>
2023-04-28 09:35:14 +0000

    url error fixed at the dashboard and analytic

--- ads/urls.py
+++ ads/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from ads.views import AdListCreateAPIView , AdRetrieveUpdateDestoryAPIView ,UserProfileRetrieveUpdateAPIView , AdvertiserDashboardAPIView
+from ads.views import AdListCreateAPIView , AdRetrieveUpdateDestoryAPIView ,UserProfileRetrieveUpdateAPIView ,\
+    AdvertiserDashboardAPIView , AdAnalyticsAPIView
 
 urlpatterns = [
     path('ads/',AdListCreateAPIView.as_view(), name='ad_list_create'),
     path('ads/<int:pk>/',AdRetrieveUpdateDestoryAPIView.as_view(), name='ad_retrieve_update_destory'),
     
     path('user/profile/',UserProfileRetrieveUpdateAPIView.as_view(),name='user_profile'),
+    path('advertiser/dashboard/',AdvertiserDashboardAPIView.as_view(),name='advertiser_dashboard'),
+    path('ads/analytics/',AdAnalyticsAPIView.as_view(),name='ad_analytics'),
     
     
 ]
