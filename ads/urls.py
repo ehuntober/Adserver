e55883856abf34e67931d1c7a4b90d6ee5421483
ehuntober <ehuntercrypto@gmail.com>
2023-04-28 10:17:26 +0000

    project setting adde

--- ads/urls.py
+++ ads/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from ads.views import AdListCreateAPIView , AdRetrieveUpdateDestoryAPIView ,UserProfileRetrieveUpdateAPIView ,\
     AdvertiserDashboardAPIView , AdAnalyticsAPIView
 
+from django.conf.urls import url 
+from rest_framework_swagger
+
 urlpatterns = [
     path('ads/',AdListCreateAPIView.as_view(), name='ad_list_create'),
     path('ads/<int:pk>/',AdRetrieveUpdateDestoryAPIView.as_view(), name='ad_retrieve_update_destory'),
--- ads_management_server/settings.py
+++ ads_management_server/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'ads'
+    'ads',
+    'rest_framework_swagger',
 ]
 
 REST_FRAMEWORK = {
