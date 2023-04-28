104266bf0fac011feef42498b8a58ac31fe70a9f
ehuntober <ehuntercrypto@gmail.com>
2023-04-28 09:30:38 +0000

    added user profile urls

--- ads/urls.py
+++ ads/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from ads.views import AdListCreateAPIView , AdRetrieveUpdateDestoryAPIView
+from ads.views import AdListCreateAPIView , AdRetrieveUpdateDestoryAPIView ,UserProfileRetrieveUpdateAPIView , AdvertiserDashboardAPIView
 
 urlpatterns = [
     path('ads/',AdListCreateAPIView.as_view(), name='ad_list_create'),
-    path('ads/<int:pk>/',AdRetrieveUpdateDestoryAPIView.as_view(), name='ad_retrieve_update_destory')
+    path('ads/<int:pk>/',AdRetrieveUpdateDestoryAPIView.as_view(), name='ad_retrieve_update_destory'),
+    
+    path('user/profile/',UserProfileRetrieveUpdateAPIView.as_view(),name='user_profile'),
+    
+    
 ]
