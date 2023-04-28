a385304e375c2a814d5528e98dbe4700a5a5d8e2
ehuntober <ehuntercrypto@gmail.com>
2023-04-28 09:38:22 +0000

    database modified

--- ads/admin.py
+++ ads/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Ad  
+from .models import Ad  ,UserProfile
 
 admin.site.register(Ad)
+admin.site.register(UserProfile)
